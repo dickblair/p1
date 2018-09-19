63cf1219f9a2a24883c7873d83540a8a41bafec5
Dick Blair <major.dick.blair@gmail.com>
2018-09-19 18:39:00 -0500

    Added a stylesheet to style the picture

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang='eng'>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
 <body>
 
 <?php
 echo "My first PHP script!";
 ?>
-<img src='images/dickpic.jpg' title='Dick Blair' align=center/>
+<img src='images/dickpic.jpg' title='Dick Blair' class="center"/>
 
 
 </body>
